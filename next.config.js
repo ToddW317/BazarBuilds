b63bbe190554af577b8d470065779557f129761e
Todd W. <117058912+ToddW317@users.noreply.github.com>
2024-11-04 00:36:47 -0800

    nextconfig.js file creation

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    missingSuspenseWithCSRInDEV: false,
+  }
 }
 
 module.exports = nextConfig 
